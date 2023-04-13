6c58a6215275c8d5670cd86c7304c96427893987
Yury Stolarz <yurysscareer@gmail.com>
2023-04-13 18:17:30 -0300

    feat: add req 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -28,7 +28,14 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    try:
+        soup = BeautifulSoup(html_content, "html.parser")
+        next_page = soup.find("a", {"class": "next page-numbers"})['href']
+    except TypeError:
+        return None
+
+    else:
+        return next_page
 
 
 # Requisito 4
