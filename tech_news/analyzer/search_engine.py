9a95d967be0955f8312f0fca7da063c68ae1e540
Yury Stolarz <yurysscareer@gmail.com>
2023-04-14 20:08:32 -0300

    feat: add req 8 and 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -15,9 +16,28 @@ def search_by_title(title: str):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        get_date = datetime.strptime(date, "%Y-%m-%d")
+        date_format = datetime.strftime(get_date, "%d/%m/%Y")
+
+        news = search_news({"timestamp": date_format})
+
+        return [
+            (info["title"], info["url"])
+            for info in news
+        ]
+
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = search_news({
+        "category": {"$regex": category, "$options": "i"},
+    })
+
+    return [
+        (info["title"], info["url"])
+        for info in news
+    ]
